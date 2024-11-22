e428ddbeb9f781ef03e2983c47ca59eb62fdc06f
Olek <alexlikenew@gmail.com>
2024-11-22 08:33:37 +0100

    Preloader configuration

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const preloader = document.querySelector('#preloader');
-  const preloaderFunc = () => {
-    
-    setTimeout(function () {
-        console.log(preloader);
-       preloader.style.opacity = "0";
-      }, 1720);
-  }
-  preloaderFunc();
+const preloader = document.querySelector('#preloader')
+const preloaderFunc = () => {
+	setTimeout(function () {
+		console.log(preloader)
+		preloader.style.display = 'none'
+	}, 1720)
+}
+preloaderFunc()
