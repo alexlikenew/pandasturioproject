2788ed53d2ed76529366f134fe4d9ffef62f2ed2
Olek <alexlikenew@gmail.com>
2024-12-06 15:00:34 +0100

    Modal window created

--- js/script.js
+++ js/script.js
@@ -8,6 +8,24 @@
 // preloaderFunc()
 
 const BOOKSY = "https://booksy.com/pl-pl/17197_panda-masaz_masaz_10729_rzeszow";
-document.querySelector(".btn-redirect-to-booksy").onclick = function () {
-    location.href = BOOKSY;
-};
+// document.querySelector(".btn-redirect-to-booksy").onclick = function () {
+//     location.href = BOOKSY;
+// };
+
+
+document.querySelectorAll("[data-dialog]").forEach(button => {
+    button.addEventListener("click", ()=> {
+        console.log("sjkpw");
+      const dialog = document.querySelector(`#${button.dataset.dialog}`);
+      dialog.showModal();
+      
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
+      
+      dialog.querySelector(".closeDialog").addEventListener("click", () => {
+        dialog.close();
+        document.body.style.removeProperty("overflow");
+        document.documentElement.style.removeProperty("overflow");
+      })
+    })
+  })
