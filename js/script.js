bfebce05513d9f09d7e2cbab0422a0e125226cd7
Olek <alexlikenew@gmail.com>
2024-12-06 13:05:20 +0100

    Pages massage & trening changed

--- js/script.js
+++ js/script.js
@@ -6,3 +6,8 @@
 // 	}, 1720)
 // }
 // preloaderFunc()
+
+const BOOKSY = "https://booksy.com/pl-pl/17197_panda-masaz_masaz_10729_rzeszow";
+document.querySelector(".btn-redirect-to-booksy").onclick = function () {
+    location.href = BOOKSY;
+};
