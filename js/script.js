ea1387c71bd00f13f0e1a7e9b43058ee9815b4bd
Olek <alexlikenew@gmail.com>
2024-12-07 10:42:56 +0100

    Contact page completed

--- js/script.js
+++ js/script.js
@@ -7,25 +7,24 @@
 // }
 // preloaderFunc()
 
-const BOOKSY = "https://booksy.com/pl-pl/17197_panda-masaz_masaz_10729_rzeszow";
+const BOOKSY = 'https://booksy.com/pl-pl/17197_panda-masaz_masaz_10729_rzeszow'
 // document.querySelector(".btn-redirect-to-booksy").onclick = function () {
 //     location.href = BOOKSY;
 // };
 
+document.querySelectorAll('[data-dialog]').forEach(button => {
+	button.addEventListener('click', () => {
+		console.log('sjkpw')
+		const dialog = document.querySelector(`#${button.dataset.dialog}`)
+		dialog.showModal()
 
-document.querySelectorAll("[data-dialog]").forEach(button => {
-    button.addEventListener("click", ()=> {
-        console.log("sjkpw");
-      const dialog = document.querySelector(`#${button.dataset.dialog}`);
-      dialog.showModal();
-      
-      document.body.style.overflow = "hidden";
-      document.documentElement.style.overflow = "hidden";
-      
-      dialog.querySelector(".closeDialog").addEventListener("click", () => {
-        dialog.close();
-        document.body.style.removeProperty("overflow");
-        document.documentElement.style.removeProperty("overflow");
-      })
-    })
-  })
+		document.body.style.overflow = 'hidden'
+		document.documentElement.style.overflow = 'hidden'
+
+		dialog.querySelector('.closeDialog').addEventListener('click', () => {
+			dialog.close()
+			document.body.style.removeProperty('overflow')
+			document.documentElement.style.removeProperty('overflow')
+		})
+	})
+})
